e2ea8281ca29768ee1db9ed104f375b4098b6116
VladiTitov <capt.Hoffmann@gmail.com>
2022-12-11 13:51:04 +0300

    feat: IProperty interface for pipe was added

--- Frontend/src/app/services/housing.service.ts
+++ Frontend/src/app/services/housing.service.ts
@@ -1,14 +1,28 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { map } from 'rxjs';
+import { IProperty } from '../property/IProperty.Interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HousingService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  getAllProperties(){
-    return this.http.get('data/properties.json')
+  getAllProperties() : Observable<IProperty[]> {
+    return this.http.get('data/properties.json').pipe(
+      map(data => {
+        const array = data as Array<IProperty>;
+        const propertiesArray: Array<IProperty> = [];
+        for (const id in array) {
+          if (array.hasOwnProperty(id)){
+            propertiesArray.push(array[id]);
+          }
+        }
+        return propertiesArray;
+      })
+    );
   }
 }
