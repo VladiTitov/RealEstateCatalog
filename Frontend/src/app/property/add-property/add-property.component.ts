65e4aa8df0e1d6c54ce7e696530cb0138b5f2b5d
VladiTitov <capt.Hoffmann@gmail.com>
2022-12-17 12:24:24 +0300

    feat: template driven form implemented

--- Frontend/src/app/property/add-property/add-property.component.ts
+++ Frontend/src/app/property/add-property/add-property.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-property',
@@ -8,7 +9,7 @@ import { Router } from '@angular/router';
 })
 
 export class AddPropertyComponent implements OnInit {
-
+  @ViewChild('Form') addPropertyForm: NgForm;
   constructor(private router: Router) { }
 
   ngOnInit() {
@@ -16,6 +17,10 @@ export class AddPropertyComponent implements OnInit {
 
   onBack(){
     this.router.navigate(['/']);
+  }
+
+  onSubmit(){
+    console.log(this.addPropertyForm);
   }
 
 }
