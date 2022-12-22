98b0ea63290c205e361539f1603e80fd0e81f2b7
Vladislav Titov <capt.Hoffmann@gmail.com>
2022-12-22 13:19:36 +0300

    feat: new property form was added

--- Frontend/src/app/property/add-property/add-property.component.ts
+++ Frontend/src/app/property/add-property/add-property.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { TabsetComponent } from 'ngx-bootstrap/tabs';
+import { IProperty } from '../IProperty.Interface';
 
 @Component({
   selector: 'app-add-property',
@@ -12,6 +13,18 @@ import { TabsetComponent } from 'ngx-bootstrap/tabs';
 export class AddPropertyComponent implements OnInit {
   @ViewChild('Form') addPropertyForm: NgForm;
   @ViewChild('formTabs') formTabs: TabsetComponent;
+
+  propertyView : IProperty = {
+    Id: 0,
+    SellRent: 0,
+    Name: '',
+    Type: '',
+    Price: 0
+  };
+
+  propertyTypes: Array<string> = ['House', 'Apartment', 'Duplex'];
+  furnishTypes: Array<string> = ['Fully', 'Semi', 'Unfurnished'];
+  moveTypes: Array<string> = ['East', 'West', 'South', 'North']
   constructor(private router: Router) { }
 
   ngOnInit() {
@@ -27,6 +40,10 @@ export class AddPropertyComponent implements OnInit {
 
   selectTab(tabId: number) {
     this.formTabs.tabs[tabId].active = true;
+  }
+
+  getString(index: number){
+    return index.toString();
   }
 
 }
