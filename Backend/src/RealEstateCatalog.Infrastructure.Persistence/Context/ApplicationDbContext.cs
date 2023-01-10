e3d05221047fb00aa954af2c1d57bfe127059724
VladiTitov <capt.Hoffmann@gmail.com>
2023-01-10 13:51:01 +0300

    refactor: a rudimentary code was deleted

--- Backend/src/RealEstateCatalog.Infrastructure.Persistence/Context/ApplicationDbContext.cs
+++ Backend/src/RealEstateCatalog.Infrastructure.Persistence/Context/ApplicationDbContext.cs
@@ -4,15 +4,8 @@
 public class ApplicationDbContext : DbContext
 {
 	public DbSet<City> Cities { get; set; }
+    public DbSet<User> Users { get; set; }
 
 	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-	{
-		
-	}
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<City>().Property(i=>i.Id).UseIdentityAlwaysColumn();
-        base.OnModelCreating(modelBuilder);
-    }
+	{ }
 }
